b831a9e3ce05761b5118a893e61d08bbb2015e27
André Gonçalves <2k9andre@gmail.com>
2022-01-23 18:20:18 +0000

    More Cleaning

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,27 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import * as client from '../assets/client'
+import { startHeader } from '../src/header'
 
 export default defineComponent({
   name: 'Home',
   mounted () {
-    client.getElement()
+    startHeader()
   }
 })
 </script>
+
+<style>
+  body{
+    margin: 0%;
+    overflow: hidden;
+  }
+
+  .headCanvas{
+    position: absolute;
+    margin: 0%;
+    size: cover;
+    width: 100%;
+    height: 100%;
+  }
+</style>

a9c42915cd8b01fe5285b128c9522bed8fac528f
André Gonçalves <2k9andre@gmail.com>
2022-01-23 18:20:18 +0000

    More Cleaning

--- src/src/RenderHead.ts
+++ src/src/RenderHead.ts
@@ -1,28 +1,47 @@
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import * as THREE from 'three'
 
 export class RenderHead {
   public scene : THREE.Scene;
   public renderer : THREE.WebGLRenderer;
-  public controls : OrbitControls;
+  public camera : THREE.PerspectiveCamera;
+  private static instance : RenderHead;
 
-  public constructor (camera : THREE.Camera) {
+  public static getInstance (): RenderHead {
+    if (RenderHead.instance == null) {
+      RenderHead.instance = new RenderHead()
+    }
+
+    return RenderHead.instance
+  }
+
+  constructor () {
     this.scene = new THREE.Scene()
+    this.scene.background = new THREE.Color(0x456990)
+
+    const light = new THREE.PointLight()
+    light.position.set(20, 20, 250)
+    this.scene.add(light)
+
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      10,
+      800
+    )
+    this.camera.position.set(0, 0, 5)
+
     this.renderer = new THREE.WebGLRenderer()
+    this.renderer.outputEncoding = THREE.sRGBEncoding
     this.renderer.setSize(window.innerWidth, window.innerHeight)
-    this.controls = new OrbitControls(camera, this.renderer.domElement)
-    this.controls.enablePan = false
+
+    window.addEventListener('resize', this.onWindowResize, false)
   }
 
-  public AddMeshToScene (mesh : THREE.Mesh): void {
-    this.scene.add(mesh)
-  }
-
-  public CreateCube (): THREE.Mesh {
+  CreateCube (): THREE.Mesh {
     const geometry = new THREE.BoxGeometry()
-    const material = new THREE.MeshBasicMaterial({
+    const material = new THREE.MeshToonMaterial({
       color: 0x00ff00,
-      wireframe: false
+      wireframe: true
     })
     const cube = new THREE.Mesh(geometry, material)
     this.scene.add(cube)
@@ -32,5 +51,22 @@ export class RenderHead {
 
   getDomElement (): HTMLCanvasElement {
     return this.renderer.domElement
+  }
+
+  startRendering (): void{
+    RenderHead.render()
+  }
+
+  private static render (): void{
+    const head = RenderHead.getInstance()
+    head.renderer.render(head.scene, head.camera)
+    requestAnimationFrame(RenderHead.render)
+  }
+
+  private onWindowResize (): void {
+    this.camera.aspect = window.innerWidth / window.innerHeight
+    this.camera.updateProjectionMatrix()
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    RenderHead.render()
   }
 }

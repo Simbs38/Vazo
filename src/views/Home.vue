4ed42599f909f80d83b5ec6759035b05db33eee9
André Gonçalves <2k9andre@gmail.com>
2022-01-23 15:43:09 +0000

    Update

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="headCanvas">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import * as client from '../assets/client'
+
+export default defineComponent({
+  name: 'Home',
+  mounted () {
+    client.getElement()
+  }
+})
+</script>

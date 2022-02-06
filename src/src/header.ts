3f8748561a86569c4d905aa212183163b25ea9cf
André Gonçalves <2k9andre@gmail.com>
2022-02-06 20:02:19 +0000

    Minor Fixes

--- src/src/header.ts
+++ src/src/header.ts
@@ -7,7 +7,7 @@ import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass'
 import { Text2Mesh } from './Text2Mesh'
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.set(0, 1.1, 2)
+camera.position.set(0, 1.1, 3)
 camera.lookAt(0, 0, 0)
 
 const head = new RenderHead(camera)
@@ -15,13 +15,15 @@ head.renderer.toneMapping = THREE.ReinhardToneMapping
 head.renderer.toneMappingExposure = 2
 head.renderer.shadowMap.enabled = true
 
-const fontPath = 'https://raw.githubusercontent.com/Simbs38/Vazo/improve/simbsText/src/fonts/Bebas%20Neue_Regular.json'
+const outlineObjects = new Array<THREE.Object3D>()
+const fontPath = 'https://raw.githubusercontent.com/Simbs38/Vazo/master/src/fonts/Bebas%20Neue_Regular.json'
 
 Text2Mesh.LoadFont(fontPath).then(font => {
     const textMesh = Text2Mesh.CreateText('Deram te o vaso?', 0.5, 0.1, 10, 1, 1, font)
     textMesh.position.set(-1.9, 0.5, -4)
     head.scene.add(textMesh)
-})
+    outlineObjects.push(textMesh)
+}).catch(err => console.log(err))
 
 const composer = new EffectComposer(head.renderer)
 composer.addPass(new RenderPass(head.scene, camera))
@@ -40,7 +42,10 @@ export function startHeader ():void {
         Obj2Mesh.ChangeToToon(args.children[0] as THREE.Mesh, 0xffffff)
         Obj2Mesh.ChangeColor(args.children[1] as THREE.Mesh, 0x17DE63)
 
-        const outlinePass = new OutlinePass(new THREE.Vector2(1024, 1024), head.scene, camera, [args.children[0], args.children[1]])
+        outlineObjects.push(args.children[0])
+        outlineObjects.push(args.children[1])
+
+        const outlinePass = new OutlinePass(new THREE.Vector2(1024, 1024), head.scene, camera, outlineObjects)
         outlinePass.visibleEdgeColor.set(0xffffff)
         outlinePass.edgeThickness = 2
         outlinePass.edgeStrength = 10
